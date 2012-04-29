eba62bec86e3638160c3a6b9b9342e1875c6c0d7
Ty Lacock <tylacock@gmail.com>
2012-04-29 17:22:23 -0400

    added form validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  validates :title, :image_url, :description, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)$}i,
+  																				 					 message: 'must be a URL to GIF, JPG or PNG'}
+
 end
